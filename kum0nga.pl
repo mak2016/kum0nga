7fb36578f1d29bb8c04eb379dda7864a77660e9e
X-C3LL <overloadblog@hotmail.es>
2015-04-28 18:24:25 +0200

    Create kum0nga.pl

---
+++ kum0nga.pl
@@ -0,0 +1,67 @@
+# Kum0nga Joomla Scan v0.1
+
+# Coded by: X-C3LL
+
+# Blog: 0verl0ad.net || ka0labs.net
+# Twitter: @TheXC3LL
+
+
+use Getopt::Long;
+use LWP::UserAgent;
+
+push(@INC, '.');
+require 'lib/version.pl';
+#require 'lib/extensiones.pl';
+
+GetOptions(
+	"url=s" => \$flag_url,
+	"version" => \$flag_version,
+	"user-agent=s" => \$flag_useragent,
+	"enum-vul" => \$flag_enum_vul,
+);
+
+
+
+banner();
+
+unless ($flag_url) { help(); }
+unless ($flag_useragent) { $flag_useragent = "Scanned by Kum0nga"; }
+if ($flag_version) { version(); }
+
+
+sub banner {
+print q(
+
+[===================================================]
+           ____                      ,
+          /---.'.__             ____//
+               '--.\           /.---'
+          _______  \\         //
+        /.------.\  \|      .'/  ______
+       //  ___  \ \ ||/|\  //  _/_----.\__
+      |/  /.-.\  \ \:|< >|// _/.'..\   '--'
+         //   \'. | \'.|.'/ /_/ /  \\
+        //     \ \_\/" ' ~\-'.-'    \\
+       //       '-._| :H: |'-.__     \\
+      //           {/'==='\}'-._\     ||
+      ||                        \\    \|
+      ||                         \\    '
+      |/                          \\
+                                   ||
+    Kum0nga v0.1                   ||
+    Joomla! Scanner                \\
+                                    '
+[==================================================]
+
+);
+}
+
+sub help {
+print q(
+Usage: perl kumonga.pl <URL> <OPTIONS>
+
+OPTIONS:
+   --version   => Check Joomla! version
+   --enum-vul  => Enumerate vulnerable extensions installed
+);
+}
